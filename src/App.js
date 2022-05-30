cb47a81d4bd02eb1635e3109142c6697c28192fb
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-05-31 04:56:57 +0800

    Router & Page Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+//importing components
+import Home from "./page/home/Home";
+import Create from "./page/create/Create";
+import Recipe from "./page/recipe/Recipe";
+import Search from "./page/search/Search";
+
+//styles
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/page/recipe/Recipe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Recipe.css'
+
+export default function Recipe() {
+  return (
+    <div>Recipe</div>
+  )
+}
