194585134a641a564854843bd6325ac1852b1a1a
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-05 05:31:56 +0800

    Fetching data in FIreStore

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,5 +1,5 @@
-import { useFetch } from "../../hooks/useFetch";
-
+import { projectFirestore } from "../../firebase/config";
+import { useEffect, useState } from "react";
 //components
 import RecipeList from "../../components/RecipeList";
 
@@ -7,7 +7,33 @@ import RecipeList from "../../components/RecipeList";
 import "./Home.css";
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("Recipes")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("No recipes to load");
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results)
+          setIsPending(false)
+        }
+      }).catch(err => {
+        setError(err.message)
+        setIsPending(false)
+      })
+  }, []);
 
   return (
     <div className="home">
