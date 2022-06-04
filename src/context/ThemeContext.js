81c80f6b71851dde1b7295b69e8743d977b8422e
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-05 04:07:10 +0800

    useReducer for changing the nav Color

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,11 +1,29 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-export const ThemeContext = createContext()
+export const ThemeContext = createContext();
 
-export function ThemeProvider({ children}) {
- return (
-     <ThemeContext.Provider value={{ color: 'blue'}}>
-        {children}
-     </ThemeContext.Provider>
- )
+//The main brain of the color data will work
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return { ...state, color: action.payload };
+    default:
+      return state;
+  }
+};
+// Where color Data will store
+export function ThemeProvider({ children }) {
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: "blue",
+  });
+//where the color of onClick Data will store 
+  const changeColor = (color) => {
+      dispatch({type: 'CHANGE_COLOR', payload: color})
+  }
+
+  return (
+    <ThemeContext.Provider value={{ ...state, changeColor }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
