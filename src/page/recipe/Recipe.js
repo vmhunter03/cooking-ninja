7baab75e4e59ca82f65455d1bbce4e131c8e784b
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-06 10:35:21 +0800

    Update Function

--- src/page/recipe/Recipe.js
+++ src/page/recipe/Recipe.js
@@ -17,11 +17,10 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
+    const unsub = projectFirestore
       .collection("Recipes")
       .doc(id)
-      .get()
-      .then((doc) => {
+      .onSnapshot((doc) => {
         if (doc.exists) {
           setIsPending(false);
           setRecipe(doc.data());
@@ -30,8 +29,13 @@ export default function Recipe() {
           setError("Could not find that recipe");
         }
       });
+    return () => unsub();
   }, [id]);
-
+  const handleClick = () => {
+    projectFirestore.collection("Recipes").doc(id).update({
+      title: "Updated the Title",
+    });
+  };
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
@@ -46,6 +50,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
